fba1f51f3799f6f97fc9e3ca16f99841eee9fa88
Cassie Boyd <cassandra.bacon@gmail.com>
2020-03-11 16:07:50 -0500

    Finished models

--- ponyproject/ponyapp/models/pictures.py
+++ ponyproject/ponyapp/models/pictures.py
@@ -1,11 +1,11 @@
 from django.db import models
-from 
+from .user_pony import UserPony
 
 class Picture (models.Model):
 
     img_path = models.CharField(max_length=255)
     caption = models.CharField(max_length=255)
-    user_pony = models.ForeignKey()
+    user_pony = models.ForeignKey(UserPony, verbose_name=("user_pony"), on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = ("Picture")
--- ponyproject/ponyapp/models/pony.py
+++ ponyproject/ponyapp/models/pony.py
@@ -1,6 +1,7 @@
 from django.db import models
 from .country import Country
-
+from .body_style import BodyStyle
+from .generation import Generation
 
 class Pony (models.Model):
 
@@ -8,8 +9,10 @@ class Pony (models.Model):
     year = models.IntegerField(max_length=4)
     hair_color = models.CharField(max_length=80)
     body_color = models.CharField(max_length=80)
+    img_path = models.CharField(max_length=255)
     country = models.ForeignKey(Country, verbose_name=("Country"), on_delete=models.DO_NOTHING)
     body_style = models.ForeignKey(BodyStyle, verbose_name=("body_style"), on_delete=models.DO_NOTHING)
+    generation = models.ForeignKey(Generation, verbose_name=("generation"), on_delete=models.DO_NOTHING)
 
     class Meta:
         verbose_name = ("pony")
