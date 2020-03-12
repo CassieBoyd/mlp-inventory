bfc8a7bd9b61ae44e40fb246c5baec09e117dc13
Cassie Boyd <cassandra.bacon@gmail.com>
2020-03-12 09:38:01 -0500

    Starting fixtures

---
+++ ponyproject/ponyapp/models/__init__.py
@@ -0,0 +1,7 @@
+from .body_style import BodyStyle
+from .condition import Condition
+from .country import Country
+from .generation import Generation
+from .picture import Picture
+from .pony import Pony
+from .user_pony import UserPony
--- ponyproject/ponyapp/models/user_pony.py
+++ ponyproject/ponyapp/models/user_pony.py
@@ -5,12 +5,12 @@ from .pony import Pony
 
 class UserPony (models.Model):
 
-    price = models.DecimalField()
+    price = models.DecimalField(decimal_places=2, max_digits=7)
     details = models.CharField(max_length=255)
     timestamp = models.DateTimeField( auto_now=False, auto_now_add=True)
     condition = models.ForeignKey(Condition, verbose_name=("condition"), on_delete=models.DO_NOTHING)
     user = models.ForeignKey(User, verbose_name=("User"), on_delete=models.CASCADE)
-    pony = models.ForeignKey(Pony, verbose_name=_("pony"), on_delete=models.DO_NOTHING)
+    pony = models.ForeignKey(Pony, verbose_name=("pony"), on_delete=models.DO_NOTHING)
 
     class Meta:
         verbose_name = ("user_pony")
