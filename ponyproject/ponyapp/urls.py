887472ac9b106ea1b2bbc2acb3b2c5c657f9d090
Cassie Boyd <cassandra.bacon@gmail.com>
2020-03-13 15:00:06 -0500

    Added pony_details and added url path

--- ponyproject/ponyapp/urls.py
+++ ponyproject/ponyapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import *
+from django.urls import path
 
 app_name = "ponyapp"
 
 urlpatterns = [
     path('', home, name='home'),
     path('ponies/', pony_list, name='ponies'),
+    path('ponies/<int:pony_id>/', pony_details, name='pony'),
 ]
