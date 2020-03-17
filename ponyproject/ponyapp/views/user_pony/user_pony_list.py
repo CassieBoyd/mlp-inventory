639c75a805cf1b84fb7a338bb1c568fbef4e27aa
Cassie Boyd <cassandra.bacon@gmail.com>
2020-03-17 12:23:07 -0500

    User-specific ponies showing for real this time instead of grabbing all user ponies

--- ponyproject/ponyapp/views/user_pony/user_pony_list.py
+++ ponyproject/ponyapp/views/user_pony/user_pony_list.py
@@ -5,7 +5,7 @@ from ponyapp.models import UserPony
 
 def user_pony_list(request):
     if request.method == 'GET':
-        all_ponies = UserPony.objects.all()
+        all_ponies = UserPony.objects.filter(user_id = request.user.id)
         template = 'user_ponies/list.html'
         context = {
             'all_ponies': all_ponies
