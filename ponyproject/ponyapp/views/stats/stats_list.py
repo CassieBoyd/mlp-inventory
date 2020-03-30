291eb3ef859ecb2376cabcdce9e29418458a5ca0
Cassie Boyd <cassandra.bacon@gmail.com>
2020-03-30 16:33:43 -0500

    Added query for count and fixed total value to only show the user-specific total instead of all prices in the database

--- ponyproject/ponyapp/views/stats/stats_list.py
+++ ponyproject/ponyapp/views/stats/stats_list.py
@@ -1,16 +1,18 @@
 import sqlite3
 from django.shortcuts import render
 from ponyapp.models import UserPony
-from django.db.models import Sum
+from django.db.models import Sum, Count
 
 def total_value(request):
     if request.method == 'GET':
 
-        total = UserPony.objects.aggregate(sum=Sum('price'))['sum']
+        total = UserPony.objects.filter(user__id=request.user.id).aggregate(sum=Sum('price'))['sum']
+        ponies_owned = UserPony.objects.filter(user__id=request.user.id).count()
 
         template = 'stats/list.html'
         context = {
-            'total': round(total,2)
+            'total': round(total,2),
+            'ponies_owned' : ponies_owned
         }
 
         return render(request, template, context)
